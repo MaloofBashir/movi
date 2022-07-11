d88cbb8b0713ad5f708c0127cffa8332c3cb4d66
Maloof <44017606+MaloofBashir@users.noreply.github.com>
2022-07-11 20:24:57 +0530

    Update settings.py

--- mov/settings.py
+++ mov/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0-ecxbxmcld4u$)r_(9n9$1oe+nj^&3tx)==0jrnk6um5%!*v#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
